fd356b6aba0f304005741ef2a507a4700b84134c
sleepymwangi <pmwangi0610@gmail.com>
2023-02-18 01:02:40 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - initialization of character
+ * Description: putchar to print alphabet
+ * Return:0 succession
+ */
+int main(void)
+{
+	char A;
+
+	for (A = 'a'; A <= 'z'; A++)
+	{
+		putchar(A);
+	}
+	return (0);
+}
