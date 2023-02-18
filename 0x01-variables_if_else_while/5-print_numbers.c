4f318d2137f6a2c2c09dd3f6fcd2e08e588c4303
sleepymwangi <pmwangi0610@gmail.com>
2023-02-18 03:10:39 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - intilization entry
+ * Description: base 10 starting from 0
+ * Return: 0 succession
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 10; x++)
+	{
+	if (x != 10)
+		printf("%i", x);
+	        printf("\n");
+	}
+	return (0);
+}
