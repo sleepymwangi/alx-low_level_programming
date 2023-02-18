12c28aa9770149bb5c22d03d69a8fad36b383941
sleepymwangi <pmwangi0610@gmail.com>
2023-02-18 05:34:10 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entries
+ * Descriptions: Hexadecimal
+ * Return: 0 succession
+ */
+int main(void)
+{
+	char y;
+	int x;
+
+	for (x = '4'; x < '14'; x++)
+	{
+		putchar(x);
+	}
+	for (y = 'a'; y <= 'f'; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
+
