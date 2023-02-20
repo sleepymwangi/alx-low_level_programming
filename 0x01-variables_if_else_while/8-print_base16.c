fc413e5207e8e614134d2b4add80d1577835fe47
sleepymwangi <pmwangi0610@gmail.com>
2023-02-20 08:51:11 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,6 +7,12 @@
  */
 int main(void)
 {
-       int c = 0;
-       char x = 'a';
+	int a, b;
 
+	for (a = 48; a <= 57; a++)
+		putchar(a);
+	for (b = 97; b <= 102; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
