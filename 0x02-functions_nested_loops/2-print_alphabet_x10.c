090b7cc5b51b2055a93729f7eced6ed0d47d8cb8
sleepymwangi <pmwangi0610@gmail.com>
2023-02-21 05:03:42 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function
+ *
+ * void - Entries
+ *
+ * prints alphabets 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	char c;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
