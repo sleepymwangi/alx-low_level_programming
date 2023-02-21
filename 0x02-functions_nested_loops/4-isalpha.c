87ea3abe7e6910658be6e07ce5f60ae436cafacf
sleepymwangi <pmwangi0610@gmail.com>
2023-02-21 08:29:31 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main"
+
+/**
+ * _isalpha - print lowercase or uppercase
+ * @c: is c a character
+ * Return: 1
+ * Returns 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
