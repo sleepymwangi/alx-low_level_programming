59b9465fdb917d80e50abea9aec749342ac89f59
sleepymwangi <pmwangi0610@gmail.com>
2023-02-22 08:16:35 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - rints all natural numbers from n to 98
+ * @n: print from this number
+ * Prototype: void print_to_98(int n);
+ * Numbers must be separated by a comma, followed by a space
+ * Numbers should be printed in order
+ * The last printed number should be 98
+ * Return: 0 succession
+ */
+void print_to_98(int n)
+{
+	int x, y;
+
+	if (n <= 98)
+	{
+		for (x = n; n <= 98; n++)
+		{
+			if (x != 98)
+			{
+				printf("%d, ", x);
+			}
+			else if (x == 98)
+			{
+				printf("%d\n", x)
+			}
+		}
+		else if (n >= 98)
+		{
+			for (y = n; y >= 98; y++)
+			{
+				if (y != 98)
+				{
+					printf("%d, ", y);
+				}
+				else if (y == 98)
+				{
+					printf("%d\n", y);
+				}
+
+			}
+
+		}
+}
