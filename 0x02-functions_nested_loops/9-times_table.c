201e62e1c7129458aa31947bb5a7ac933062e9aa
sleepymwangi <pmwangi0610@gmail.com>
2023-02-22 07:20:48 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,15 +22,15 @@ void times_table(void)
 			if (z < 10 && y != 0)
 			{
 				_putchar(',');
-				_putchar('.');
-				_putchar('.');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(z + '0');
 			}
 			else
 				if (z >= 10)
 				{
 					_putchar(',');
-					_putchar('.');
+					_putchar(' ');
 					_putchar((z / 10) + '0');
 					_putchar((z % 10) + '0');
 				}
