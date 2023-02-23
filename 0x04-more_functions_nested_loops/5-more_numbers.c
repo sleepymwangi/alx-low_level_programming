59e6802d620257bf00559224433eb5bc635bd244
sleepymwangi <pmwangi0610@gmail.com>
2023-02-23 04:33:30 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,12 @@
  */
 void more_numbers(void)
 {
-	for (int x = 0; x < 10; x++)
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
 	{
-		for (int y = 0; y < 15; y++)
+		for (y = 0; y < 15; y++)
 		{
 			if (y >= 10)
 				_putchar(y / 10 + '0');
