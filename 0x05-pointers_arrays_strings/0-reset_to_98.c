2deebfa96d0753a3f378c8c9ff5c05a598359f65
sleepymwangi <pmwangi0610@gmail.com>
2023-02-27 02:26:54 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value *n points to.
+ * @n: the number to modify.
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98; /* the value pointed by 'n' upgraded to 98 */
+
+}
