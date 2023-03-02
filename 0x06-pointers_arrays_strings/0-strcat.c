ce8e02c25fad117d04c560f8f30ab4619a8c3812
sleepymwangi <pmwangi0610@gmail.com>
2023-03-01 23:19:27 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,14 +13,13 @@ char *_strcat(char *dest, char *src)
 	int y;
 
 	for (x = 0; dest[x] != '\0'; x++)
-	{
-	}
+		;
 	for (y = 0; src[y] != '\0';  y++)
 	{
-		if (dest[x] == src[y])
+		dest[x] = src[y];
 		x++;
-		y++;
 	}
+
 	dest[x] = '\0';
 	return (dest);
 }
