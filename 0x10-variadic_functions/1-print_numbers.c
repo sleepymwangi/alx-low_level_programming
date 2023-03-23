c1354fc52ede5523d1c0e4b8cac8283511b7c943
sleepymwangi <pmwangi0610@gmail.com>
2023-03-23 03:01:32 -0700

    make it clear and separate

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers with a newline.
+ * @n: number of params
+ * @separator: separator btwn two no.
+ * Return: Always 0
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list is;
+
+	va_start(is, n);
+	if (separator == NULL)
+		separator = '\0';
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%d", va_arg(is, int));
+		if (i < n - 1)
+			printf("%s", separator);
+		i++;
+	}
+	printf("\n");
+	va_end(is);
+}
