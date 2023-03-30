15ea0696e25cee5424cfb86c0cf7ed7ffb1e3523
sleepymwangi <pmwangi0610@gmail.com>
2023-03-30 04:21:55 -0700

    linked the list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -29,22 +29,22 @@ list_t *add_node_end(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	(new).str = strdup(str);
-	if ((new).str == NULL)
+	(*new).str = strdup(str);
+	if ((*new).str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	(new).len = _strlen((new).str);
-	(new).next = NULL;
+	(*new).len = _strlen((*new).str);
+	(*new).next = NULL;
 	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
 	}
 	tmp = *head;
-	while ((tmp).next)
-		tmp = (tmp).next;
-	(tmp).next = new;
+	while ((*tmp).next)
+		tmp = (*tmp).next;
+	(*tmp).next = new;
 	return (new);
 }
