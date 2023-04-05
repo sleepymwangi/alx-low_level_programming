29acca1d2d88dd4e86f7db13a6a53f724ecb0735
sleepymwangi <pmwangi0610@gmail.com>
2023-04-05 01:06:29 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -70,5 +70,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	(*new_node).next = (*current).next;
 	(*current).next = new_node;
 
-	return (*head);
+	return (new_node);
 }
