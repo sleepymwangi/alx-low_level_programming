20b8840e890eb7860ac15d55e5d745697ea347f6
sleepymwangi <pmwangi0610@gmail.com>
2023-04-05 03:59:53 -0700

    deletion at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = (*current).next;
 		free(current);
-		return (-1);
+		return (1);
 	}
 	while (current != NULL && count < index)
 	{
