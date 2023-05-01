061af07800b0e62eb6bd928fe7e0b734be777887
sleepymwangi <pmwangi0610@gmail.com>
2023-05-01 02:03:07 -0700

    reversal

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,17 +10,15 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
-	listint_t *next;
 	listint_t *current = *head;
 
 	while (current != NULL)
 	{
-		next = current->next;
-		current->next = prev;
+		listint_t *next = (*current).next;
+		(*current).next = prev;
 		prev = current;
 		current = next;
 	}
 	*head = prev;
-
 	return (*head);
 }
